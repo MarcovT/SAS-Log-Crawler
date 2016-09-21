f1b07da971438c7a8e73714bb9f8546f60b10d57
MarcovT <Marco.vanTonder@quintiles.com>
2016-09-21 16:12:06 +0200

    Updated Functions

--- LogCrawl.py
+++ LogCrawl.py
@@ -7,19 +7,19 @@ def getSettingsRegex():
   sublime.save_settings('SAS_Log_Crawler.sublime-settings')
   return err_regx
 
-def goToNextError():
+def goToNextError(theView, err_regx):
   # Get end of last cursur position
   curr_pos = 0
-  for region in self.view.sel():
+  for region in theView.sel():
     curr_pos = region.end()
 
   # Find next error/message in log
-  next_error = self.view.find(err_regx, curr_pos, sublime.IGNORECASE)
+  next_error = theView.find(err_regx, curr_pos, sublime.IGNORECASE)
   if next_error:
     # Clear out any previous error/messages in selection
-    self.view.sel().clear()
-    self.view.sel().add(next_error)
-    self.view.show(next_error)
+    theView.sel().clear()
+    theView.sel().add(next_error)
+    theView.show(next_error)
     sublime.status_message("Found issue at " + str(next_error))
   else:
     sublime.status_message("No issues found!")
@@ -29,18 +29,20 @@ class log_crawl(sublime_plugin.TextCommand):
     # Get pattern from settings or use default
     err_regx =  getSettingsRegex()
     # Go to next error
-    goToNextError()
+    goToNextError(self.view, err_regx)
 
+# Still need TODO:
 class side_bar_check_log(sublime_plugin.TextCommand):
   def run(self, paths = []):
     # Get pattern from settings or use default
     err_regx =  getSettingsRegex()
     # Go to next error
-    goToNextError()
+    goToNextError(self.view, err_regx)
 
+# Still need TODO:
 class side_bar_check_folder_logs(sublime_plugin.TextCommand):
   def run(self, paths = []):
     # Get pattern from settings or use default
     err_regx =  getSettingsRegex()
     # Go to next error
-    goToNextError()
+    goToNextError(self.view, err_regx)
