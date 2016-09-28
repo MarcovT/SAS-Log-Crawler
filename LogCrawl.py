2ecf661300ebdecf9aa98b79edb9a218471a7fe7
MarcovT <marco.vant@gmail.com>
2016-09-28 20:29:56 +0200

    Added threading and enable single log report

--- LogCrawl.py
+++ LogCrawl.py
@@ -2,6 +2,12 @@ import sublime
 import sublime_plugin
 import re
 import os
+import threading
+
+# List for storing all file names with issues
+found_files = []
+# Dict for storing associations with satisfied lines and files
+found_lines = {}
 
 default_pattern = "will be overwritten by data set|Missing values were generated|repeats of BY values|uninitialized|[^l]remerge|Invalid data for"
 
@@ -50,11 +56,12 @@ def go_to_next_error_view(theView, err_regx):
 
 
 def get_extension(filename):
-    if filename != None:
+    if filename is not None:
         path = os.path.splitext(filename)[1]
     else:
         path = None
     return path
+
 
 def check_if_can_call(view):
     # Set small asyncronous timeout while view is loading
@@ -76,15 +83,119 @@ def check_log_view(view):
     go_to_next_error_view(view, err_regx)
 
 
+def find_issue_in_line(line, regex):
+    # Find next error/message in log
+    is_issue = re.search(r'%s' % regex, line, re.IGNORECASE)
+    if is_issue:
+        # If issue is found then add it to the array of issues for the file
+        return True
+    else:
+        return False
+
+
+def create_report(found_files=[], found_lines={}, path=""):
+    print("Creating Report ...")
+    sublime.status_message("Creating Report ...")
+    # Now we build the bulky string before we create a new view and add the region
+    report = "LOG REPORT \n" + "Folder: " + path + "\n"
+    if len(found_lines) > 0:
+        # Now we show issues for each file that is of importance
+        for file in found_files:
+            report += "\nISSUES FOR FILE: " + str(file) + "\n"
+            for line in found_lines[file]:
+                report += line + "\n"
+    else:
+        report += "\nResult: NO ISSUES FOUND!"
+
+    # Now let's display the report in a view
+    newView = sublime.active_window().new_file()
+    newView.set_name("LOG REPORT.log")
+    newView.run_command("insert", {"characters": report})
+    # Trying to add scope to the new file to ensure colour coding
+    p1 = 0
+    p2 = newView.size()
+    newView.set_syntax_file("SASLog.tmLanguage")
+    print("Report Created ...")
+    sublime.status_message("Report Created ...")
+
+
+def checkSingleFile(file, path=""):
+    # Store file extension
+    extension = get_extension(file)
+    if extension == ".log":
+        # Open the file in current iteration
+        with open(path + "/" + file) as fp:
+            print("Checking File " + str(file))
+            sublime.status_message("Checking File " + str(file))
+            # We need to keep track of the lines that are issues in this file
+            issueLines = []
+            # Check every line in the opened file
+            for line in fp:
+                regex = get_settings_regex(extension)
+                # Check if line satisfies the regex pattern stored in settings
+                issueLine = find_issue_in_line(line, regex)
+                # If an issue is found then store the line in an array
+                if issueLine:
+                    if file not in found_files:
+                        found_files.append(file)
+                    # Add the line and associate it with its parent file
+                    issueLines.append(line)
+                else:
+                    continue
+        if len(issueLines) > 0:
+            found_lines[file] = issueLines
+    else:
+        return False
+
+
+class singleLogThread (threading.Thread):
+    def __init__(self, view, args, logObj):
+        threading.Thread.__init__(self)
+        self.view = view
+        self.args = args
+        self.logObj = logObj
+
+    def run(self):
+        # Get dirs and files from args
+        dirs = self.args["dirs"]
+        files = self.args["files"]
+        # Stores the folder path of the files
+        if len(dirs) > 0:
+            path = dirs[0]
+        else:
+            path = os.path.dirname(files[0])
+        # Get dirs and files from args
+        file = files[0]
+        checkSingleFile(file)
+        # Create Final Report
+        create_report(found_files, found_lines, path)
+
+
+class folderLogThread (threading.Thread):
+    def __init__(self, view, args):
+        threading.Thread.__init__(self)
+        self.view = view
+        self.args = args
+
+    def run(self):
+        # Get dirs and files from args
+        dirs = self.args["dirs"]
+        files = self.args["files"]
+        # Stores the folder path of the files
+        if len(dirs) > 0:
+            path = dirs[0]
+        else:
+            path = os.path.dirname(files[0])
+
+        # Loop through files to check each file's lines
+        for file in os.listdir(path):
+            checkSingleFile(file, path)
+        # Create Final Report
+        create_report(found_files, found_lines, path)
+
+
 class log_crawl(sublime_plugin.TextCommand):
     def run(self, edit):
-        check_log_view(self.view)
-
-
-# Still need TODO:
-class side_bar_check_log(sublime_plugin.TextCommand):
-
-    def run(self, view, files=[]):
         file = paths[0]
         if file:
             # Open the file that was selected with the cursur
@@ -95,85 +206,17 @@ class side_bar_check_log(sublime_plugin.TextCommand):
             sublime.status_message("No file selected.")
 
 
-# Still need TODO:
-class side_bar_check_folder_logs(sublime_plugin.TextCommand):
-
-    def find_issue_in_line(self, line, regex, current=0, max=0):
-        # Find next error/message in log
-        is_issue = re.search(r'%s' % regex, line, re.IGNORECASE)
-        if is_issue:
-            # If issue is found then add it to the array of issues for the file
-            sublime.status_message("Checking File " + str(current) + " of " + str(max))
-            return True
-        else:
-            return False
-
-    def create_report(self, found_files=[], found_lines={}, path=""):
-        # Now we build the bulky string before we create a new view and add the region
-        report = "LOG REPORT \n" + "Folder: " + path + "\n"
-        if len(found_lines) > 0:
-            # Now we show issues for each file that is of importance
-            for file in found_files:
-                report += "\nISSUES FOR FILE: " + str(file) + "\n"
-                for line in found_lines[file]:
-                    report += line + "\n"
-        else:
-            report += "\nResult: NO ISSUES FOUND!"
-
-        # Now let's display the report in a view
-        newView = sublime.active_window().new_file()
-        newView.set_name("LOG REPORT.log")
-        newView.run_command("insert", {"characters": report})
-        # Trying to add scope to the new file to ensure colour coding
-        p1 = 0
-        p2 = newView.size()
-        newView.set_syntax_file("TempLogSyntax.tmLanguage")
-        # newView.add_regions('log_report', [sublime.Region(p2, p1)], 'source.SASLog', 'dot', sublime.HIDDEN)
+class side_bar_check_log(sublime_plugin.TextCommand):
 
     def run(self, view, **args):
-        # Get dirs and files from args
-        dirs = args["dirs"]
-        files = args["files"]
-        # List for storing all file names with issues
-        found_files = []
-        # Dict for storing associations with satisfied lines and files
-        found_lines = {}
-        # Stores the folder path of the files
-        if len(dirs) > 0:
-            path = dirs[0]
-        else:
-            path = os.path.dirname(files[0])
+        thread1 = singleLogThread(self.view, args, self)
+        thread1.start()
 
-        # Counter to count the current position of the loop
-        current = 0
-        # Counter to hold the top value of the range for files found
-        max = len(files)
 
-        # Loop through files to check each file's lines
-        for file in os.listdir(path):
-            # Store file extension
-            extension = get_extension(file)
-            if extension == ".log":
-                # Open the file in current iteration
-                with open(path + "/" + file) as fp:
-                    # We need to keep track of the lines that are issues in this file
-                    issueLines = []
-                    # Check every line in the opened file
-                    for line in fp:
-                        regex = get_settings_regex(extension)
-                        current = current + 1
-                        # Check if line satisfies the regex pattern stored in settings
-                        issueLine = self.find_issue_in_line(line, regex, current, max)
-                        # If an issue is found then store the line in an array
-                        if issueLine:
-                            if file not in found_files:
-                                found_files.append(file)
-                            # Add the line and associate it with its parent file
-                            issueLines.append(line)
-                        else:
-                            continue
-                if len(issueLines) > 0:
-                    found_lines[file] = issueLines
-            else:
-                continue
-        self.create_report(found_files, found_lines, path)
+class side_bar_check_folder_logs(sublime_plugin.TextCommand):
+
+    def run(self, view, **args):
+        thread2 = folderLogThread(view, args)
+        thread2.start()
+        print("Started Checking Folder Logs ...")
+        sublime.status_message("Started Checking Folder Logs ...")
